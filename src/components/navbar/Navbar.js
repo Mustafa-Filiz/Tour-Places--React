4a7643f18441f630ab5f2cd4c18640e7823288db
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-10-01 17:03:13 +0300

    updates

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -5,12 +5,12 @@ const Navbar = () => {
     return (
         <div className="container">
             <ul className="navBar">
-                <li><a href="#">About Us</a></li>
-                <li><a href="#">For You</a></li>
-                <li><a href="#">Services</a></li>
-                <li><a href="#">Blog</a></li>
-                <li><a href="#">Vlog</a></li>
-                <li><a href="#">Contact</a></li>
+                <li><a>About Us</a></li>
+                <li><a>For You</a></li>
+                <li><a>Services</a></li>
+                <li><a>Blog</a></li>
+                <li><a>Vlog</a></li>
+                <li><a>Contact</a></li>
             </ul>
         </div>
     )
